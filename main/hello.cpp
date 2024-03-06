331d31f61cee82ce5a6f6c1802bdfd16d02c7cb8
anjanrp <anjanprasad.21@gmail.com>
2024-03-06 10:06:46 +0530

    WebHook Commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+    cout << "Web-Hook added for CC Lab on March 6 2024 at 10:05 a.m\n";
   return 0;
 }
